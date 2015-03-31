43d1ece227a2a2c1e5ca594785bd5bd0111834ae
Joep Kerste <joep_ned@live.nl>
2015-03-31 21:24:54 +0200

    selected pizzas are now added to order list

--- app/src/main/java/com/example/etienne/pizzapp/ConfirmOrder.java
+++ app/src/main/java/com/example/etienne/pizzapp/ConfirmOrder.java
@@ -8,16 +8,24 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 
 public class ConfirmOrder extends ActionBarActivity {
+
+    ArrayList<String> selectedPizzas = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
 
+        if(!this.getIntent().getStringArrayListExtra("selectedPizzas").isEmpty())
+        {
+            selectedPizzas = this.getIntent().getStringArrayListExtra("selectedPizzas");
+        }
+
         ListView lv = (ListView)findViewById(R.id.pizzasListView);
-        String[] selectedPizzas = getResources().getStringArray(R.array.selectedPizzas_array);
         lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, selectedPizzas));
 
         Button confirmButton = (Button)findViewById(R.id.confirmButton);
@@ -35,6 +43,7 @@ public class ConfirmOrder extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Intent addIntent = new Intent(v.getContext(), SelectPizza.class);
+                addIntent.putExtra("selectedPizzas", selectedPizzas);
                 v.getContext().startActivity(addIntent);
             }
         });
--- app/src/main/java/com/example/etienne/pizzapp/SelectPizza.java
+++ app/src/main/java/com/example/etienne/pizzapp/SelectPizza.java
@@ -1,5 +1,7 @@
 package com.example.etienne.pizzapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,15 +12,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 
 public class SelectPizza extends ActionBarActivity {
 
     ListView lv;
+    ArrayList<String> selectedPizzas = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_pizza);
+
+        if(this.getIntent().getStringArrayListExtra("selectedPizzas") != null)
+        {
+            selectedPizzas = this.getIntent().getStringArrayListExtra("selectedPizzas");
+        }
 
         lv = (ListView)findViewById(R.id.listView2);
         String[] previousOrders = getResources().getStringArray(R.array.pizzas_array);
@@ -26,7 +38,11 @@ public class SelectPizza extends ActionBarActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedPizza = lv.getItemAtPosition(position).toString();
+
                 Intent confirmIntent = new Intent(view.getContext(), ConfirmOrder.class);
+                selectedPizzas.add(selectedPizza);
+                confirmIntent.putExtra("selectedPizzas", selectedPizzas);
                 view.getContext().startActivity(confirmIntent);
             }
         });
